f84facea86ed7094741dacac6aacaf49094c6bea
biba228Alekc <149318339+biba228Alekc@users.noreply.github.com>
2023-10-29 22:34:28 +0300

    Delete быки и коровы directory

--- быки и коровы/functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-void generateSecretNumber(int secret[]);
-
-
-int isValidDigit(int secret[], int index, int digit);
-
-
-void calculateBullsAndCows(int secret[], int guess[], int* bulls, int* cows);
