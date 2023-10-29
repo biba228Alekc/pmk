2ab01f9ae55fdd818aab6d521c4163251291b6e9
biba228Alekc <149318339+biba228Alekc@users.noreply.github.com>
2023-10-29 22:43:47 +0300

    Add files via upload

---
+++ быки и коровы/functions.h
@@ -0,0 +1,8 @@
+#pragma once
+void generateSecretNumber(int secret[]);
+
+
+int isValidDigit(int secret[], int index, int digit);
+
+
+void calculateBullsAndCows(int secret[], int guess[], int* bulls, int* cows);
