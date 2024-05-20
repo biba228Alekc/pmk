3f21ef008eec0830a18d81c7d0962a46f6b0726c
biba228Alekc <149318339+biba228Alekc@users.noreply.github.com>
2024-05-21 01:34:53 +0300

    Header.h

--- 8й номер ЭЙлера/Header.h
+++ 8й номер ЭЙлера/Header.h
@@ -7,4 +7,4 @@ void calculate_composition(const char* num, int sequence);
 long long my_strlen(const char* str);
 
 
-void measure_time(void (*func)(const char*, int), const char* num, int sequence);
+void time_func(void (*func)(const char*, int), const char* num, int sequence);
