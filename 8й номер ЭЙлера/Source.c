fe5c99e089d4ba0a4bdd30215f9fe3c249aa1843
biba228Alekc <149318339+biba228Alekc@users.noreply.github.com>
2024-05-21 01:35:38 +0300

    Source.c

--- 8й номер ЭЙлера/Source.c
+++ 8й номер ЭЙлера/Source.c
@@ -4,7 +4,8 @@
 #include "Header.h"
 
 
-int main() {
+int main() 
+{
     char num[] = "73167176531330624919225119674426574742355349194934"
         "96983520312774506326239578318016984801869478851843"
         "85861560789112949495459501737958331952853208805511"
@@ -26,9 +27,8 @@ int main() {
         "05886116467109405077541002256983155200055935729725"
         "71636269561882670428252483600823257530420752963450";
 
-  
-    int sequence = 13;
-    measure_time(calculate_composition, num, sequence);
+  int sequence = 13;
+  time_func(calculate_composition, num, sequence);
 
     return 0;
 }
