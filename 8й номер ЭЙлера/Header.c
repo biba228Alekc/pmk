80749db2521328c210634a420cac3fb757f55d51
biba228Alekc <149318339+biba228Alekc@users.noreply.github.com>
2024-05-21 01:15:55 +0300

    Add files via upload

---
+++ 8й номер ЭЙлера/Header.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+
+long long my_strlen(const char* str) {
+    long long length = 0;
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    return length;
+}
+
+
+void calculate_composition(const char* num, int sequence) {
+    long long composition = 0;
+    long long length = my_strlen(num);//количество символов в строке
+
+    for (int i = 0; i <= length-sequence; i++) //цикл идет до конца строки
+    {
+        long long mult = 1;
+        for (int j = 0; j < sequence; j++) //умножает по 13 элементов в строке
+        {
+            mult *= num[i + j] - '0';// Преобразуем символ цифры в числовое значение по индексу i+j ;
+        }
+        if (mult > composition) 
+        {
+            composition = mult;
+        }
+    }
+    printf("Otvet %lld\n", composition);
+}
+
+// Функция для измерения времени работы другой функции
+void measure_time(void (*func)(const char* , int), const char* num, int sequence) {
+    clock_t start, end;
+    double time;
+
+    start = clock();
+    func(num , sequence); 
+    end = clock();
+
+    time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("TIME: %f sec\n", time);
+}
---
+++ 8й номер ЭЙлера/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+void calculate_composition(const char* num, int sequence);
+
+
+long long my_strlen(const char* str);
+
+
+void measure_time(void (*func)(const char*, int), const char* num, int sequence);
